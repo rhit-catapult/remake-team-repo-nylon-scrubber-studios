830be119cade4585f30534d2ff1d712e63489853
caesarsaladisg84u <caesarsaladisg84u@gmail.com>
2026-06-26 14:02:46 -0400

    Update camera_system.py

--- camera_system.py
+++ camera_system.py
@@ -1,20 +1,25 @@
 import sys
 import pygame
 import camera
+import counselor_module
 
 class Camera_System:
-    def __init__(self, screen, x, y, next_camera):
+    def __init__(self, screen, camera_on):
         self.screen = screen
+        self.current_camera = 0
+        self.camera_on = camera_on
         #self.minimap_image = pygame.image.load("")
-        self.cameras = [1 , 2, 3, 4, 5, 6]
-        self.minimap_x = x
-        self.minimap_y = y
-        self.next_camera = next_camera
+        self.cameras = []
+        self.camera_image_folders = ["","","","","",""]
         for i in range(6):
+            self.cameras.append(camera.Camera())
 
-    def Draw_Minimap(self):
-        self.screen.blit(self.minimap_image, (self.minimap_x, self.minimap_y))
+    def Draw_Minimap(self, minimap_x, minimap_y):
+        self.screen.blit(self.minimap_image, (minimap_x, minimap_y))
     def Camera_On_or_Off(self):
-        
-    def Switch_Camera(self):
-        
+        if self.camera_on:
+            self.Draw_Minimap
+    def Are_Counselors_There(self):
+        counselor_module.counselor_locations
+    def Switch_Camera(self, camera_to_switch_to):
+        camera.Camera.draw(camera_to_switch_to)
