b3a19ebfadf661b0208824eaa00655d58184704b
Endherbrine <aidenwang9@outlook.com>
2026-06-26 18:59:18 -0400

    added more to button module, office module, and project.py
    added the aiman button placeholder
    the screen has a display
    you can move left and right by hovering over the two sides

--- project.py
+++ project.py
@@ -3,7 +3,7 @@ import sys
 import random
 import time
 import os
-import camera_system
+import camera_system_module
 import button_module
 import office_module
 
@@ -11,9 +11,13 @@ def main():
     # turn on pygame
     pygame.init()
     pygame.display.set_caption("One Night at Catapult")
-    screen = pygame.display.set_mode(800,600)
+    screen = pygame.display.set_mode((800,600))
     #screen = pygame.display.set_mode((800, 600), pygame.FULLSCREEN | pygame.SCALED)
-    camera_sys = camera_system.Camera_System(screen,False)
+    camera_sys = camera_system_module.Camera_System(screen,False)
+    office = office_module.Office(screen,"images/test_picture.png",True)
+    left_rect = pygame.Rect(0,0,40,600)
+    right_rect = pygame.Rect(760,0,40,600)
+    aiman_button = button_module.Buttons(screen,50,450,"images/button_test.png")
 
     # let's set the framerate
     clock = pygame.time.Clock()
@@ -22,9 +26,24 @@ def main():
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 sys.exit()
+            if aiman_button.rect.collidepoint(pygame.mouse.get_pos()):
+                if event.type == pygame.MOUSEBUTTONDOWN:
+                    print("clicked")
 
+
+        #office side scrolling
+        if left_rect.collidepoint(pygame.mouse.get_pos()):
+            office.move("left")
+        elif right_rect.collidepoint(pygame.mouse.get_pos()):
+            office.move("right")
+
+        
+        
 
         screen.fill((255, 255, 255))
+
+        office.draw()
+        aiman_button.draw()
 
 
 
