e6384a07b9269281dee3e66fd0997af2f6c7ddad
caesarsaladisg84u <caesarsaladisg84u@gmail.com>
2026-06-26 13:34:08 -0400

    Update camera.py

--- camera.py
+++ camera.py
@@ -1,1 +1,16 @@
-#"TEST TEST TEST"
+import sys
+import pygame
+
+class Camera:
+    def __init__(self, screen, current_camera, camera_image):
+        self.screen = screen
+        
+        self.camera_image = pygame.image.load(camera_image)
+        self.counselors_in_camera = []
+        self.counselors_images = []
+
+    def draw(self):
+
+        pass
+    def is_Counselor_There(self):
+        pass
