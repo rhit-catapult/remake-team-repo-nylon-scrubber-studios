f7bf6d055da52da44a15f8c1fb6b5cb44f865386
Endherbrine <aidenwang9@outlook.com>
2026-06-26 13:48:16 -0400

    update

--- camera.py
+++ camera.py
@@ -1,16 +1,21 @@
 import sys
 import pygame
+import os
 
 class Camera:
-    def __init__(self, screen, current_camera, camera_image):
+    def __init__(self, screen: pygame.Surface, camera_image,image_folder,counselor_camera):
         self.screen = screen
-        
         self.camera_image = pygame.image.load(camera_image)
-        self.counselors_in_camera = []
+        self.counselors_in_camera = counselor_camera
         self.counselors_images = []
+        for image in os.listdir(image_folder):
+            self.counselors_images.append(pygame.image.load(image))
 
     def draw(self):
+        self.screen.blit(self.camera_image,(0,0))
 
-        pass
-    def is_Counselor_There(self):
-        pass
+    def is_counselor_there(self):
+        for couns in self.counselors_in_camera:
+            if couns == "carp":
+                self.screen.blit()
+
