eba0d776ae2c956a224cef8402285e633fd4dac6
caesarsaladisg84u <caesarsaladisg84u@gmail.com>
2026-06-30 11:10:31 -0400

    counselor position fixes

--- image pos test.py
+++ image pos test.py
@@ -7,18 +7,38 @@ pygame.init()
 screen = pygame.display.set_mode((800, 600))
 pygame.display.set_caption("Image Test")
 
-camera_object = camera_module.Camera(screen, "images/cam6_main.jpg", 100,20)
-image_test = pygame.image.load("images/cam6_main.jpg")
+camera_object = camera_module.Camera(screen, "images/cam1_main.jpg", 100,20)
+image_test = pygame.image.load("images/cam1_main.jpg")
 image_test = pygame.transform.scale(image_test, (800, 600))
 
 
 while True:
     screen.fill((0, 0, 0))
     screen.blit(image_test, (0, 0))
-    camera_object.draw_counselor("images/aiman/aiman_cam6.png", (-30,100),(800,600))
+    camera_object.draw_counselor("images/andrew/andrew_cam1.png", (-60,-10),(800,600))
     pygame.display.update()
 
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             pygame.quit()
             sys.exit()
+
+
+
+
+
+# DON'T DELETE THE FOLLOWING:
+# draw_counselor("images/jj/jj_pos1.png", (0,-1.9),(800,600))
+# draw_counselor("images/jj/jj_pos2.png", (14,-35),(800,600))
+# draw_counselor("images/jj/jj_pos3.png", (15,-29),(800,600))
+# draw_counselor("images/andrew/andrew_cam1.png", (-60,-10),(800,600))
+
+# draw_counselor("images/carp/carp_cam2.png", (-100,70),(800,600))
+
+# draw_counselor("images/carp/carp_cam3_pos1.png", (-5,140),(800,600))
+# draw_counselor("images/carp/carp_cam3_pos2.png", (5,150),(800,600))
+
+# draw_counselor("images/andrew/andrew_cam4_pos1.png",(0,15),(800,600))
+# draw_counselor("images/andrew/andrew_cam4_pos2.png",(-10,30),(800,600))
+
+# draw_counselor("images/aiman/aiman_cam6.png", (-30,100),(800,600))
